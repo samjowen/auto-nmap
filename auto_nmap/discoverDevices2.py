5369d71c2cee40b4f0a04df782f4f0edb81a97d9
Samuel Owen <SamHirael@gmail.com>
2023-03-05 01:09:58 +0000

    refactor: clean discoverDevices2.py

--- auto_nmap/discoverDevices2.py
+++ auto_nmap/discoverDevices2.py
@@ -1,27 +1,24 @@
-from auto_nmap.Scan import Scan
 import argparse
 import sys
+from auto_nmap.Scan import Scan
+
 
 def parse_cmdline():
     """CLI tool to add parameters to the script."""
     parser = argparse.ArgumentParser(description='discoverPorts of a device.')
     parser.add_argument("--output", metavar="FILEPATH",
-                        nargs=1, help="Filepath to .txt file of IP addresses to scan.", required=False)
+                        nargs=1, help="Filepath to .txt of IP addresses to scan.", required=False)
     args = parser.parse_args()
     return args
 
+
 def main():
-    args = parse_cmdline()
-    if not len(sys.argv) > 1:
+    """Main function to run the script."""
+    if len(sys.argv) <= 1:
         scan = Scan()
         router_ip = scan.get_default_gateway_ip()
         scan.target_ip = scan.ip_to_ip_range(router_ip)
         scan.nmap_ping_scan(ip=scan.target_ip)
-
-"""
-Todo: Implement the output to a file funtionality.
-"""
-
 
 
 if __name__ == '__main__':
